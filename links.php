d9a7f108b56d9efca7745d82d558c2c9c202421e
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2023-10-03 13:00:12 +0300

    доработка logs для links

--- links.php
+++ links.php
@@ -5,9 +5,9 @@ require __DIR__ . "/vendor/autoload.php";
 use DiDom\Document;
 use GuzzleHttp\Client as GuzzleClient;
 
-try {
-    sleep(mt_rand(5, 10));
+echo "<b>скрипт начал работу " . date("d-m-Y H:i:s", time()) . "</b><br><br>";
 
+try {
     //Получаем ссылку, с которой будем парсить
     $query = sql("SELECT link, views FROM links WHERE type='catalog' ORDER BY views, id LIMIT 1");
     if ($query->num_rows) {
@@ -25,7 +25,7 @@ try {
         'GET',
         $url
     );
-    echo "скрипт проходил ссылку $url<br><br>";
+    echo '<b>скрипт проходил ссылку <a href="$url">' . $url . '</a></b><br><br>';
 
     //Если проблема с ссылкой отправляем лог в БД и прекращаем работу скрипта
     if ($response->getStatusCode() != 200) {
@@ -41,7 +41,8 @@ try {
     $product_res = $document->find('a[href*=product]');
     $all_res = array_merge($catalog_res, $product_res);
 
-echo "скрипт нашел ссылки: <br>";
+    echo "<b>скрипт нашел ссылки:</b><br>";
+    $add = [];
     foreach ($catalog_res as $href) {
         $link = "https://mosplitka.ru" . $href->attr('href');
         echo "$link<br>";
@@ -50,26 +51,32 @@ echo "скрипт нашел ссылки: <br>";
         if (preg_match("#https://mosplitka.ru/catalog.+#", $link) and !preg_match("#.php$#", $link)) {
             try {
                 sql("INSERT INTO links (link, views, type, product_views) VALUES ('$link', 0, 'catalog', 0)");
-                echo "была добавлена ссылка $link<br><br>";
+                $add[] = $link;
             } catch (Exception $e) {
                 continue; //для дублей
-                }
+            }
 
             //добавляем ссылки с type=product
         } elseif (preg_match("#https://mosplitka.ru/product.+#", $link)) {
             try {
                 $link = mysqli_real_escape_string(getDB(), $link);
+                $add[] = $link;
                 sql("INSERT INTO links (link, views, type, product_views) VALUES ('$link', 0, 'product', 0)");
-                echo "была добавлена ссылка $link<br><br>";
             } catch (Exception $e) {
                 continue; //для дублей
             }
         }
     }
-    
-        echo "не было ошибок<br><br>";
+    echo "<br><b>из них скрипт добавил:</b><br>";
+    foreach ($add as $i) {
+        echo "$i<br>";
+    }
+    echo "<br><b>не было ошибок</b><br><br>";
 } catch (Throwable $e) {
     writeLog($e);
-    echo "была ошибка<br><br>";
+    echo "<b>была ошибка</b><br><br>";
     var_dump($e);
 }
+
+
+echo "<b>скрипт закончил работу " . date("d-m-Y H:i:s", time()) . "</b><br><br>";
