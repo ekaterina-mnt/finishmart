de0d33607ace2f89d119589761f0713e623235e4
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2023-10-04 13:35:42 +0300

    бОльшая оптимизация поиска ссылок, чтобы не было лишних id

--- links.php
+++ links.php
@@ -10,7 +10,7 @@ echo "<b>скрипт начал работу " . date("d-m-Y H:i:s", time()) . 
 try {
     //Получаем ссылку, с которой будем парсить
     try {
-        $query = sql("SELECT link, views FROM links WHERE type='catalog' ORDER BY views, id LIMIT 1");
+        $query = sql("SELECT link, views FROM test_links WHERE type='catalog' ORDER BY views, id LIMIT 1");
     } catch (Throwable $e) {
         //Если too_many_connections
         echo "<b>ошибка: </b>";
@@ -22,26 +22,35 @@ try {
         $res = mysqli_fetch_assoc($query);
         $url = $res['link'];
         $views = $res['views'] + 1;
-        sql("UPDATE links SET views=$views WHERE link='$url'");
+        //sql("UPDATE links SET views=$views WHERE link='$url'");
     } else {
         $url = "https://mosplitka.ru/catalog"; //для самого первого запуска
     }
-
-    //Получаем html у себя
-    $client = new GuzzleClient();
-    $response = $client->request(
-        'GET',
-        $url
-    );
+    
+    //тест
+    $url = 'https://mosplitka.ru/catalog/svet/';
+    
     echo '<b>скрипт проходил ссылку <a href="' . $url . '">' . $url . '</a></b><br><br>';
 
-    //Если проблема с ссылкой отправляем лог в БД и прекращаем работу скрипта
-    if ($response->getStatusCode() != 200) {
-        writeCustomLog("Код у GuzzleClient не 200. Ссылка, которую парсим - $url");
-        echo "<b>ошибка: код ссылки != 200</b><br><br>";
-        echo "<b>скрипт закончил работу " . date("d-m-Y H:i:s", time()) . "</b><br><br>";
+    //Получаем html у себя
+    try {
+        $client = new GuzzleClient();
+        $response = $client->request(
+            'GET',
+            $url
+        );
+    } catch (Throwable $e) {
+        //Если проблема с ссылкой (чаще всего 502) отправляем лог в БД 
+        writeLog($e);
+        echo "<b>ошибка: </b><br>";
+        var_dump($e);
+
+        //снова уменьшаем просмотры, чтобы скрипт еще раз прошел ссылку и прекращаем работу скрипта
+        $views -= 1;
+        sql("UPDATE links SET views=$views WHERE link='$url'");
+        echo "<br><br><b>скрипт закончил работу " . date("d-m-Y H:i:s", time()) . "</b><br><br>";
         exit();
-    };
+    }
 
     //Получаем все данные со страницы
     $document = $response->getBody()->getContents();
@@ -51,7 +60,7 @@ try {
     $product_res = $document->find('a[href*=product]');
     $all_res = array_merge($catalog_res, $product_res);
 
-    echo "<b>скрипт нашел ссылки:</b><br>";
+    echo "<b>скрипт нашел ссылки (" . count($all_res) . "шт):</b><br>";
     $add = [];
     foreach ($all_res as $href) {
         $link = "https://mosplitka.ru" . $href->attr('href');
@@ -64,7 +73,7 @@ try {
         }
 
         //избавляемся от дублей
-        if (sql("SELECT id, link FROM links WHERE link='$link'")->num_rows) {
+        if (sql("SELECT id, link FROM test_links WHERE link='$link'")->num_rows) {
             continue;
         };
 
@@ -79,16 +88,17 @@ try {
         if (isset($type)) {
             try {
                 $link = mysqli_real_escape_string(getDB(), $link);
-                sql("INSERT INTO links (link, views, type, product_views) VALUES ('$link', 0, '$type', 0)");
+                sql("INSERT INTO test_links (link, views, type, product_views) VALUES ('$link', 0, '$type', 0)");
                 $add[] = $link;
             } catch (Exception $e) {
                 continue; //для дублей
             }
         }
     }
-    echo "<br><b>из них скрипт добавил:</b><br>";
-    foreach ($add as $i) {
-        echo "$i<br>";
+    sort($add);
+    echo "<br><b>из них скрипт добавил (" . count($add) . "шт):</b><br>";
+    foreach ($add as $n => $i) {
+        echo $n+1 . ") $i<br>";
     }
     echo "<br><b>не было ошибок</b><br><br>";
 } catch (Throwable $e) {
