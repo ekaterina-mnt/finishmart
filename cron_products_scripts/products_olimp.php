0588f477ba2988b179606167355adb6b46e60ee3
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-01-26 19:03:59 +0300

    1

--- cron_products_scripts/products_olimp.php
+++ cron_products_scripts/products_olimp.php
@@ -36,8 +36,7 @@ try {
 
         $url_parser = $res['link'];
         $provider = $res['provider'];
-        $url_parser = "https://olimp-parketa.ru/catalog/inzhenernaya-doska/tekhnomassiv-ot-400-do-2000kh160kh18-dub-natur-kayt/";
-
+        
         // $links = MySQL::sql("SELECT link, provider from all_products WHERE provider='" . $script_iteraion_provider . "' and subcategory IS NULL or subcategory='null' ORDER BY date_edit LIMIT 10");
         // foreach ($links as $link) {
         // sleep(mt_rand(2, 6));
