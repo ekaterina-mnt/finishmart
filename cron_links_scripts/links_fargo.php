2d05b755be577c7dc8bba86b68f3d758d4f1cc37
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-01-26 20:33:02 +0300

    1

--- cron_links_scripts/links_fargo.php
+++ cron_links_scripts/links_fargo.php
@@ -21,7 +21,7 @@ $script_iteration_provider = 'fargo';
 TechInfo::start();
 
 try {
-    for ($i = 1; $i < 6; $i++) {
+    for ($i = 1; $i < 2; $i++) {
 
         echo "<br><b>Ссылка $i</b><br><br>";
         //Получаем ссылку, с которой будем парсить
