d464b4d9fe5f06f86adde3836b40592d28890c1f
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-01-27 19:47:30 +0300

    1

--- functions/GoogleSheets/ParseCharacteristics/Napolnye.php
+++ functions/GoogleSheets/ParseCharacteristics/Napolnye.php
@@ -2,7 +2,7 @@
 
 namespace functions\GoogleSheets\ParseCharacteristics;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use functions\Parser;
 
