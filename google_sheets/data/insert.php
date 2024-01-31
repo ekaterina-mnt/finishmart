c82b5c641af4c4017a8a0610fa5b1cb97661c221
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-01-31 22:54:00 +0300

    1

--- google_sheets/data/insert.php
+++ google_sheets/data/insert.php
@@ -61,9 +61,9 @@ try {
   Sheet::update_data($specific_attributes_cell, $specific_attributes);
 
   if ($filled_ids_str) {
-    $query = "SELECT * FROM all_products WHERE subcategory like '{$needed_subcategory}' AND category like '{$needed_category}' AND id NOT IN ($filled_ids_str) AND (status like 'ok' OR status IS NULL)";
+    $query = "SELECT * FROM all_products WHERE subcategory like '{$needed_subcategory}' AND category like '{$needed_category}' AND id NOT IN ($filled_ids_str) AND (status like 'ok' OR status IS NULL) AND char_views > 0";
   } else {
-    $query = "SELECT * FROM all_products WHERE subcategory like '{$needed_subcategory}' AND category like '{$needed_category}' AND (status like 'ok' OR status IS NULL)";
+    $query = "SELECT * FROM all_products WHERE subcategory like '{$needed_subcategory}' AND category like '{$needed_category}' AND (status like 'ok' OR status IS NULL) AND char_views > 0";
   }
   $goods = MySQL::sql($query);
 
