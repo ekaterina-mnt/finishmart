4574b827b6768166436f0b72986425db1738d1ee
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-02-16 15:55:20 +0300

    1

--- functions/GoogleSheets/Goods/GetGoods.php
+++ functions/GoogleSheets/Goods/GetGoods.php
@@ -13,10 +13,16 @@ class GetGoods
 {
     static function getGoods($filled_ids_str, $needed_subcategory, $needed_category)
     {
-        if ($filled_ids_str) {
-            $query = "SELECT * FROM all_products WHERE subcategory like '{$needed_subcategory}' AND category like '{$needed_category}' AND id NOT IN ($filled_ids_str) AND (status like 'ok' OR status IS NULL) AND char_views > 0";
+        // if ($filled_ids_str) {
+        //     $query = "SELECT * FROM all_products WHERE subcategory like '{$needed_subcategory}' AND category like '{$needed_category}' AND id NOT IN ($filled_ids_str) AND (status like 'ok' OR status IS NULL) AND char_views > 0";
+        // } else {
+        //     $query = "SELECT * FROM all_products WHERE subcategory like '{$needed_subcategory}' AND category like '{$needed_category}' AND (status like 'ok' OR status IS NULL) AND char_views > 0";
+        // }
+
+                if ($filled_ids_str) {
+            $query = "SELECT * FROM all_products WHERE category like '{$needed_category}' AND id NOT IN ($filled_ids_str) AND (status like 'ok' OR status IS NULL) AND char_views > 0";
         } else {
-            $query = "SELECT * FROM all_products WHERE subcategory like '{$needed_subcategory}' AND category like '{$needed_category}' AND (status like 'ok' OR status IS NULL) AND char_views > 0";
+            $query = "SELECT * FROM all_products WHERE category like '{$needed_category}' AND (status like 'ok' OR status IS NULL) AND char_views > 0";
         }
 
         $goods = MySQL::sql($query);
