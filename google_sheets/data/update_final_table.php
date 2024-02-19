536df3375f3eb42521b7e024b42c06db594dd5de
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-02-19 12:58:01 +0300

    1

--- google_sheets/data/update_final_table.php
+++ google_sheets/data/update_final_table.php
@@ -20,40 +20,40 @@ try {
     echo "Скрипт начал - " . date('Y-m-d H:i:s', time()) . "<br><br>";
 
     $GoogleSheets_tablename = "napolnye_raw";
+    $tablename = "final_products";
+    // $quaries = 
 
-    $queries = Sheet::get_data("AW4:AW", $GoogleSheets_tablename);
+    // include("add_final_table_columns.php"); // чтобы добавить колонки в mysql 
 
-
-    $final_query .= " ADD COLUMN `$column` TEXT(1500) DEFAULT NULL,";
-
-
-
-    // // Создать текст update-запроса
-    // $letter = "D";
-    // $str = "=\"UPDATE final_products SET ";
-    // foreach ($insert_attributes as $attr) {
-    //     if (in_array($attr, ["id в новой таблице", "id", "Дата изменения"])) continue;
-    //     $str .= $attr . '=' . "'\"&$letter" . "4&\"', ";
-    //     $letter++;
-    // }
-    // $str = substr($str, 0, -2);
-    // $str .= " WHERE id=\"C4\"\";";
-    // echo $str . "<br><br>";
+    // $queries = Sheet::get_data("AW4:AW", $GoogleSheets_tablename);
 
 
-    // // Создать тест insert-запроса
-    // $letter = "D";
-    // $str = "=\"INSERT INTO final_products (";
+
+    // Создать тест insert-запроса
+    $letter = "D";
+    $str = "=\"INSERT INTO final_products (";
     // $str .= implode(", ", array_slice($insert_attributes, 3));
-    // $str .= ") VALUES (";
-    // foreach ($insert_attributes as $attr) {
-    //     if (in_array($attr, ["id в новой таблице", "id", "Дата изменения"])) continue;
-    //     $str .= "'\"&$letter" . "4&\"', ";
-    //     $letter++;
-    // }
-    // echo $str . "<br><br>";
+    foreach ($insert_attributes as $attr) {
+        if (in_array($attr, ["id в новой таблице", "id", "Дата изменения"])) continue;
+        $str .= "'$attr', ";
+        $str_vals .= "'\"&$letter" . "4&\"', ";
+        $letter++;
+    }
+    $str = substr($str, 0, -1);
+    $str .= ") VALUES (" . $str_vals . ")";
+    echo $str . "<br><br>";
 
-
+    // Создать текст update-запроса
+    $letter = "D";
+    $str = "=\"UPDATE final_products SET ";
+    foreach ($insert_attributes as $attr) {
+        if (in_array($attr, ["id в новой таблице", "id", "Дата изменения"])) continue;
+        $str .= "'$attr'" . '=' . "'\"&$letter" . "4&\"', ";
+        $letter++;
+    }
+    $str = substr($str, 0, -2);
+    $str .= " WHERE id=\"C4\"\";";
+    echo $str . "<br><br>";
 
     echo "<br>Скрипт закончил - " . date('Y-m-d H:i:s', time());
 } catch (Throwable $e) {
