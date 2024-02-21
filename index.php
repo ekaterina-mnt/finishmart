0e60ff5e61b36f2b72496d9b0632563559192a31
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-02-21 12:31:12 +0300

    1

--- index.php
+++ index.php
@@ -7,88 +7,95 @@ use functions\GoogleSheets\ParseCharacteristics\Plitka;
 use functions\GoogleSheets\ParseCharacteristics\ConnectedSubcategories;
 use functions\TechInfo;
 
-$providers = [
-    'alpinefloor', 'ampir', 'artkera', 'centerkrasok', 'domix', 'dplintus', 'evroplast', 'finefloor',
-    'laparet', 'masterdom', 'mosplitka', 'ntceramic', 'olimp', 'surgaz', 'tdgalion', 'fargo'
-];
+try {
+    $providers = [
+        'alpinefloor', 'ampir', 'artkera', 'centerkrasok', 'domix', 'dplintus', 'evroplast', 'finefloor',
+        'laparet', 'masterdom', 'mosplitka', 'ntceramic', 'olimp', 'surgaz', 'tdgalion', 'fargo'
+    ];
 
-foreach ($providers as $provider) {
-    echo '<a href="http://penzevrv.beget.tech/cron_products_scripts/products_' . $provider . '.php">products_' . $provider . '</a>';
-    echo "<br>";
-    echo '<a href="http://penzevrv.beget.tech/cron_links_scripts/links_' . $provider . '.php">links_' . $provider . '</a>';
+    foreach ($providers as $provider) {
+        echo '<a href="http://penzevrv.beget.tech/cron_products_scripts/products_' . $provider . '.php">products_' . $provider . '</a>';
+        echo "<br>";
+        echo '<a href="http://penzevrv.beget.tech/cron_links_scripts/links_' . $provider . '.php">links_' . $provider . '</a>';
+        echo "<br><br>";
+    }
+
+    echo '<a href="http://penzevrv.beget.tech/cron_dop_scripts/check_invalide_links.php">check_invalide_links</a>';
     echo "<br><br>";
-}
+    echo '<a href="http://penzevrv.beget.tech/cron_dop_scripts/masterdomDopData.php">masterdomDopData</a>';
+    echo "<br><br>";
 
-echo '<a href="http://penzevrv.beget.tech/cron_dop_scripts/check_invalide_links.php">check_invalide_links</a>';
-echo "<br><br>";
-echo '<a href="http://penzevrv.beget.tech/cron_dop_scripts/masterdomDopData.php">masterdomDopData</a>';
-echo "<br><br>";
-
-$categories = Parser::getCategoriesList();
-$subcategories = ConnectedSubcategories::getList();
-$napolnye_subcategories = Napolnye::getSubcategories();
-$plitka_subcategories = Plitka::getSubcategories();
+    $categories = Parser::getCategoriesList();
+    $subcategories = ConnectedSubcategories::getList();
+    $napolnye_subcategories = Napolnye::getSubcategories();
+    $plitka_subcategories = Plitka::getSubcategories();
 
 ?>
 
-<?php
-TechInfo::preArray($subcategories);
-var_dump($subcategories);
-foreach ($subcategories as $category => $subcategoriesList) {
+    <?php
+    TechInfo::preArray($subcategories);
+    var_dump($subcategories);
+    foreach ($subcategories as $category => $subcategoriesList) {
 
-    echo '<H2>Вставить данные в Гугл таблицы</H2>
+        echo '<H2>Вставить данные в Гугл таблицы</H2>
 <form action="google_sheets/data/insert.php" , method="POST">
     <input type="hidden" name="category" value="<?= ' . $category . '?>">
     <select name="subcategory">';
-    foreach ($subcategoriesList as $subcategory) {
-        echo '<option value="' . $subcategory . '">' . $subcategory . '</option>';
-    }
-    echo '</select></p>
-    <p><input type="submit" value="Отправить"></p>
-</form>';
-}
-?>
-
-
-<H2>Вставить данные с объединенными характеристиками в другую таблицу</H2>
-<form action="google_sheets/data/merge_similar_chars.php" , method="POST">
-    <input type="hidden" name="category" value="<?= $categories[1] ?>">
-    <select name="subcategory">
-        <?php
-        foreach ($napolnye_subcategories as $subcategory) {
+        foreach ($subcategoriesList as $subcategory) {
             echo '<option value="' . $subcategory . '">' . $subcategory . '</option>';
         }
-        ?>
-    </select></p>
+        echo '</select></p>
     <p><input type="submit" value="Отправить"></p>
-</form>
+</form>';
+    }
+    ?>
+
+
+    <H2>Вставить данные с объединенными характеристиками в другую таблицу</H2>
+    <form action="google_sheets/data/merge_similar_chars.php" , method="POST">
+        <input type="hidden" name="category" value="<?= $categories[1] ?>">
+        <select name="subcategory">
+            <?php
+            foreach ($napolnye_subcategories as $subcategory) {
+                echo '<option value="' . $subcategory . '">' . $subcategory . '</option>';
+            }
+            ?>
+        </select></p>
+        <p><input type="submit" value="Отправить"></p>
+    </form>
 
 
 
-<br><br>
-<H2>Вставить листы в Гугл таблицы</H2>
-<form action="google_sheets/data/create_subcategory_pages.php" , method="POST">
-    <select name="category">
-        <?php
-        foreach ($categories as $category) {
-            echo '<option value="' . $category . '">' . $category . '</option>';
-        }
-        ?>
-    </select>
-    <p><input type="submit" value="Отправить"></p>
-</form>
-<br><br>
+    <br><br>
+    <H2>Вставить листы в Гугл таблицы</H2>
+    <form action="google_sheets/data/create_subcategory_pages.php" , method="POST">
+        <select name="category">
+            <?php
+            foreach ($categories as $category) {
+                echo '<option value="' . $category . '">' . $category . '</option>';
+            }
+            ?>
+        </select>
+        <p><input type="submit" value="Отправить"></p>
+    </form>
+    <br><br>
 
 
 
-<H2>Спарсить характеристики товаров в таблицу characteristics</H2>
-<form action="/alter_mysql_table/parse_characteristics/index.php" , method="POST">
-    <select name="category">
-        <?php
-        foreach ($categories as $category) {
-            echo '<option value="' . $category . '">' . $category . '</option>';
-        }
-        ?>
-    </select></p>
-    <p><input type="submit" value="Отправить"></p>
-</form>
+    <H2>Спарсить характеристики товаров в таблицу characteristics</H2>
+    <form action="/alter_mysql_table/parse_characteristics/index.php" , method="POST">
+        <select name="category">
+            <?php
+            foreach ($categories as $category) {
+                echo '<option value="' . $category . '">' . $category . '</option>';
+            }
+            ?>
+        </select></p>
+        <p><input type="submit" value="Отправить"></p>
+    </form>
+
+<?php
+
+} catch (Throwable $e) {
+    var_dump($e);
+}
