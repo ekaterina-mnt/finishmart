e5961f8801899faba103e83dcf62d056093b4c4f
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-02-22 11:12:40 +0300

    1

--- google_sheets/data/merge_similar_chars.php
+++ google_sheets/data/merge_similar_chars.php
@@ -141,10 +141,9 @@ try {
         // Объединям
         $values = array_merge($common_values, $specific_values);
         $values = array_map(fn ($value) => $value ?? "-", $values);
-        TechInfo::preArray($values);
-        var_dump($values);
         $insert_data[] = FormInsertData::get_i($list_name, $values, "B", $current_cell++);
     }
+    TechInfo::preArray($insert_data);
 
     echo "<br>Всего строк добавлено:" . count($insert_data);
     if (count($notCountedChars)) echo "<br><br>Эти характеристики не учтены в сопоставлении характеристик - " . implode(", ", array_unique($notCountedChars)) . "<br>";
