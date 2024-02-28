274c4316648356c5764c12327917c4161d084f1a
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-02-28 20:20:02 +0300

    1

--- functions/GoogleSheets/ParseCharacteristics/Categories/Santechnika.php
+++ functions/GoogleSheets/ParseCharacteristics/Categories/Santechnika.php
@@ -15,7 +15,6 @@ class Santechnika
         $subcategoriesList = Parser::getSubcategoriesList();
 
         $subcategories = [
-            // $subcategoriesList[58], // 'Багет',
             $subcategoriesList[6], //'Аксессуары для ванной комнаты',
             $subcategoriesList[2], //'Ванны',
             $subcategoriesList[26], //'Другое',
