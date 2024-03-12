1313c8e02ddfd0b889b19a7a3c8bfa3f6948ccc8
Ekaterina Penzeva <ekaterina22.03@rambler.ru>
2024-03-12 19:41:14 +0300

    1

--- google_sheets/data/update_final_table.php
+++ google_sheets/data/update_final_table.php
@@ -59,6 +59,8 @@ try {
 
     $columns = Sheet::get_data($columns_excel_range, $GoogleSheets_tablename);
     $columns = $columns['values'][0];
+    TechInfo::preArray($columns);
+    exit;
     $values = Sheet::get_data("$list_name!C4:ZA100000", $GoogleSheets_tablename);
     $values = $values['values'];
 
